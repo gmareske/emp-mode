248aad2f1e08460b0fdf6f6e84ade031483cf0ce
Griffin Mareske <gmareske@gmail.com>
2018-08-08 09:12:30 -0500

    Fix things for emacs checkdoc

--- emp.el
+++ emp.el
@@ -1,4 +1,11 @@
+;;; emp.el --- Minor mode for controlling MPRIS2-compatible players through Emacs
+
+;;; Commentary:
+;; 
+
 (require 'dbus)
+
+;;; Code:
 
 (define-minor-mode emp-mode
   "Minor mode for emp, an emacs MPRIS interface"
@@ -16,25 +23,25 @@
 
 (defcustom mpris-service
   "org.mpris.MediaPlayer2.rhythmbox"
-  "Default service for MPRIS"
+  "Default service for MPRIS."
   :type 'string
   :group 'emp)
 
 (defcustom mpris-max-timeout
   2000
-  "Default timeout for MPRIS service"
+  ."Default timeout for MPRIS service"
   :type 'integer
   :group 'emp)
 
 (defvar mpris-player-i "org.mpris.MediaPlayer2.Player"
-  "Name of the MediaPlayer2.Player instance")
+  "Name of the MediaPlayer2.Player instance.")
 (defvar mpris-get-prop-i "org.freedesktop.DBus.Properties"
-  "Name of the Freedesktop Properties Interface")
+  "Name of the Freedesktop Properties Interface.")
 (defvar mpris-player-name (clean-mpris-player-name mpris-service)
-  "Shorthand for the actual name of the program being controlled")
+  "Shorthand for the actual name of the program being controlled.")
 
 (defun find-mpris-player ()
-  "Returns a list of running MPRIS-compatible players"
+  "Return a list of running MPRIS-compatible players."
   (let ((players
 	 (seq-filter (lambda (s) (cl-search "org.mpris.MediaPlayer2" s))
 		     (dbus-call-method :session "org.freedesktop.DBus" "/org/freedesktop/DBus" "org.freedesktop.DBus" "ListNames"))))
@@ -44,7 +51,7 @@
   (cadr (split-string name "org.mpris.MediaPlayer2.")))
 
 (defun mpris-call (interface method &rest args)
-  "Calls #method with #interface on the MPRIS object specified by the custom variable mpris-service and returns the result"
+  "Call #METHOD with #INTERFACE on the MPRIS object specified by the custom variable ‘mpris-service’ and return the result."
   ;; synchronous
   (apply 'dbus-call-method
 	 (append `(:session
@@ -55,7 +62,7 @@
 		 args)))
 
 (defun mpris-get-prop (prop-name &rest args)
-  "Identical to mpris-call, but uses the freedesktop get-prop interface"
+  "Identical to mpris-call, but uses the freedesktop get-prop interface."
   (apply 'mpris-call
 	 (append `(,mpris-get-prop-i
 		   "Get"
@@ -64,14 +71,14 @@
 		 args)))
 
 (defun mpris-call-player (method &rest args)
-  "Identical to mpris-call, but uses the player interface"
+  "Identical to mpris-call, but uses the player interface."
   (apply 'mpris-call
 	 (append `(,mpris-player-i
 		   ,method)
 		 args)))
 
 (defun metadata-hash ()
-  "Returns a hash table of symbols of MPRIS properties to string values"
+  "Return a hash table of symbols of MPRIS properties to string values."
   (letrec ((helper (lambda (cur build)
 		     (if (null cur)
 			 build
@@ -84,39 +91,43 @@
 
 ;;; Interactive Functions
 (defun emp-toggle ()
-  "Toggle play on MPRIS Player"
+  "Toggle play on MPRIS Player."
   (interactive)
   (message "Toggling %s..." mpris-player-name)
   (mpris-call-player "PlayPause"))
 
 (defun emp-play ()
-  "Play on MPRIS Player"
+  "Play on MPRIS Player."
   (interactive)
   (message "Playing %s..." mpris-player-name)
   (mpris-call-player "Play"))
 
 (defun emp-pause ()
-  "Pause on MPRIS Player"
+  "Pause on MPRIS Player."
   (interactive)
   (message "Pausing %s..." mpris-player-name)
   (mpris-call-player "Pause"))
 
 (defun emp-next ()
-  "Next song on MPRIS Player"
+  "Next song on MPRIS Player."
   (interactive)
   (message "Starting next song on %s..." mpris-player-name)
   (mpris-call-player "Next"))
 
 (defun emp-prev ()
-  "Previous song on MPRIS Player"
+  "Previous song on MPRIS Player."
   (interactive)
   (message "Starting previous song on %s..." mpris-player-name)
   (mpris-call-player "Previous"))
 
 (defun emp-stop ()
-  "Stop MPRIS Player"
+  "Stop MPRIS Player."
   (interactive)
   (message "Stopping %s..." mpris-player-name)
   (mpris-call-player "Stop"))
    
 
+
+(provide 'emp)
+
+;;; emp.el ends here
